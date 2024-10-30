ff4b308b85a8e4f991ba93723740afc175f6e1d6
amytibbs16 <amytibbs16@gmail.com>
2024-10-29 21:24:46 -0400

    added tracts asset, added Promise, handle jsonp, handle tract isValid Co-authored-by: B. Sanders <bdougsand@users.noreply.github.com> Co-authored-by: Nick Griffiths <ngriffiths21@users.noreply.github.com>

---
+++ src/types.ts
@@ -0,0 +1,71 @@
+// Adapted from https://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.html#_Toc172117434
+export type CensusAddressMatch = {
+    tigerLine: {
+        side: string,
+        tigerLineId: string
+    },
+    coordinates: {
+        x: number,
+        y: number
+    },
+    addressComponents: {
+        zip: string,
+        streetName: string,
+        preType: string,
+        city: string,
+        preDirection: string,
+        suffixDirection: string,
+        fromAddress: string,
+        state: string,
+        suffixType: string,
+        toAddress: string,
+        suffixQualifier: string,
+        preQualifier: string
+    },
+    geographies: {
+        "Census Tracts": [
+            {
+"SUFFIX": string,
+"POP100": number,
+"GEOID": string,
+"CENTLAT": string,
+"BLOCK": string,
+"AREAWATER": number
+"STATE": string,
+"BASENAME": string,
+"OID": string,
+"LSADC": string,
+"INTPTLAT": string,
+"FUNCSTAT": string,
+"NAME": string,
+"OBJECTID": number,
+"TRACT": string,
+"CENTLON": string,
+"BLKGRP": string,
+"AREALAND": number,
+"HU100": number,
+"INTPTLON": string,
+"MTFCC": string,
+"LWBLKTYP": string,
+"UR": string,
+"COUNTY": string
+            }]
+        
+    }
+    matchedAddress: string
+};
+
+export type CensusApiResponse = {
+    result: {
+        input: {
+            address: { address: string },
+            benchmark: {
+                isDefault: boolean,
+                benchmarkDescription: string,
+                id: string,
+                benchmarkName: string
+            }
+        },
+        addressMatches: CensusAddressMatch[]
+    }
+}
