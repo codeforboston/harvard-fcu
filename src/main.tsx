4b7ad97781e93e9259bcd0b056e1f10076dc8770
Brian Sanders <bdougsand+cfb@gmail.com>
2025-02-04 22:35:50 -0300

    Slight restructuring

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('eligibility-root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', 
+  build: {
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
