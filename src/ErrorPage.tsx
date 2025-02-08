93c6cb394257403ec0826ff43ebd66ccae8d4bbb
IuliiaSam <iuliia.samokhvalova@gmail.com>
2025-02-07 21:36:23 -0500

    Updated styles as discussed

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -5,7 +5,7 @@ type Props = React.PropsWithChildren<object>;
 const ErrorPage: React.FC<Props> = () => {
   return (
     <>
-      <h3 className='elig-h3'>Oops</h3>
+      <h3 id='elig-h3'>Oops</h3>
       <p className='elig-p'>An error occurred while processing your request. Please refresh the page and try again.</p>
     </>
   )
--- src/SearchPageHeading.tsx
+++ src/SearchPageHeading.tsx
@@ -5,7 +5,7 @@ type Props = React.PropsWithChildren<object>;
 const SearchPageHeading: React.FC<Props> = () => {
   return (
     <>
-      <h3 className='elig-h3'>Do you live, work, worship, or attend school in one of our qualified census tracts?</h3>
+      <h3 id='elig-h3'>Do you live, work, worship, or attend school in one of our qualified census tracts?</h3>
       <p className='elig-p'>If so, you might be eligible for membership with us! Enter your address below to check and see if you qualify!</p>
     </>
   )
